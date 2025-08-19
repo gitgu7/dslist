6d6b70d40a49f28b6af5de88e7476c0b2da9c2d3
gitgu7 <guolisanto@gmail.com>
2025-08-19 16:21:02 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -16,19 +16,25 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(String genre, Integer year, Long id, String imgUrl, String longDescription
-            , String platform, String shortDescription, String title) {
+            , String platforms,Double score, String shortDescription, String title) {
         this.genre = genre;
         this.year = year;
         this.id = id;
         this.imgUrl = imgUrl;
         this.longDescription = longDescription;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.shortDescription = shortDescription;
         this.title = title;
     }
@@ -65,12 +71,20 @@ public class Game {
         this.longDescription = longDescription;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getShortDescription() {
